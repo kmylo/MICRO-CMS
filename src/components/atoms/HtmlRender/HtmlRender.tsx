9b22e66e7473d40a64db398bc079c36914122054
kmylo6 <kmylo6@gmail.com>
2023-05-18 01:02:16 -0300

    fix: HtmlRender TS

--- src/components/atoms/HtmlRender/HtmlRender.tsx
+++ src/components/atoms/HtmlRender/HtmlRender.tsx
@@ -1,10 +1,11 @@
 import DOMPurify from "dompurify";
 
 export interface HtmlRenderProps {
-  editableHtml: string;
+  editableHtml: string | undefined;
 }
 const HtmlRender = ({ editableHtml }: HtmlRenderProps) => {
-  const htmlSanatized = DOMPurify.sanitize(editableHtml);
+  const htmlSanatized =
+    (editableHtml && DOMPurify.sanitize(editableHtml)) ?? "";
   // TODO: add options
   return (
     <div className="html-render column with-border">
